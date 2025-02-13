91e0131a563fad810e8ed7899ee9ca3cf5f0aa39
ferid aqa <feridcodee@gmail.com>
2025-02-13 18:03:12 +0400

    config

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import './App.scss'
+import Mainlayout from './layout/mainlayout';
+import Home from './pages/home';
+import Error from './pages/error';
+
 
 function App() {
-
   return (
-   <>
-   
-   </>
+    <>
+    <BrowserRouter>
+          <Routes>
+            <Route element={<Mainlayout></Mainlayout>}  path="/">
+              <Route element={<Home></Home>}  index ></Route>
+              {/* <Route element={<Register></Register>}  path='/register' ></Route> */}
+              {/* <Route element={<Login></Login>}  path='/login' ></Route> */}
+              {/* <Route element={<Detail></Detail>}  path='/detail/:id' ></Route> */}
+
+            </Route>
+              <Route element={<Error></Error>}  path='*' ></Route>
+          </Routes>
+        </BrowserRouter>
+    </>
   )
 }
 
---
+++ front/src/pages/error/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.scss'
+function Error() {
+    return (
+        <div id='error'>
+            <div className="imgbox">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgnnje8wF-7U5MPPua-48jUfSiXvgeDD_diQ&s" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Error
